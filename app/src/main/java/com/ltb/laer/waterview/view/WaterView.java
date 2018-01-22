f7cecdff919e33276df4cb14d12fda4a48ef060b
laitianbing <laitianbing@pand-auto.com>
2018-01-22 13:34:12 +0800

    优化蚂蚁森林，添加注释

--- app/src/main/java/com/ltb/laer/waterview/view/WaterView.java
+++ app/src/main/java/com/ltb/laer/waterview/view/WaterView.java
@@ -30,34 +30,65 @@ import java.util.Random;
  * 创建时间: 2018/1/9
  * 创建人:  laitianbing
  * 描述:  蚂蚁森林模拟
+ * 处理思路：
+ *     ->将森林水滴作为一个总体而不是单个的view，自定义一个ViewGroup容器
+ *     ->循环创建view
+ *     ->为view随机设置位置(在一些固定的集合中随机选取，尽量保证水滴不重合)
+ *     ->为view设置一个初始的运动方向（注：由于每个view的运动方向不同，所以我选择将方向绑定到view的tag中）
+ *     ->为view设置一个初始的速度（同理：将初始速度绑定到view的tag中）
+ *     ->添加view到容器中，并缩放伴随透明度显示
+ *     ->开启handler达到view上下位移动画（注意点：这里我们需要定一个临界值来改变view的速度，到达view时而快时而慢的目的）
+ *     ->点击view后，缩放、透明度伴随位移移除水滴
+ *     ->界面销毁时停止调用handler避免内存泄漏，空指针等异常
  */
-
 public class WaterView extends FrameLayout {
     private static final int WHAT_ADD_PROGRESS = 1;
-    /**view变化的y抖动范围*/
+    /**
+     * view变化的y抖动范围
+     */
     private static final int CHANGE_RANGE = 10;
-    /**控制抖动动画执行的快慢，人眼不能识别16ms以下的*/
+    /**
+     * 控制抖动动画执行的快慢，人眼不能识别16ms以下的
+     */
     public static final int PROGRESS_DELAY_MILLIS = 12;
-    /**控制移除view的动画执行时间*/
+    /**
+     * 控制移除view的动画执行时间
+     */
     public static final int REMOVE_DELAY_MILLIS = 2000;
-    /**添加水滴时动画显示view执行的时间*/
+    /**
+     * 添加水滴时动画显示view执行的时间
+     */
     public static final int ANIMATION_SHOW_VIEW_DURATION = 500;
-    /**控制水滴动画的快慢*/
+    /**
+     * 控制水滴动画的快慢
+     */
     private List<Float> mSpds = Arrays.asList(0.5f, 0.3f, 0.2f, 0.1f);
-    /**x最多可选取的随机数值*/
+    /**
+     * x最多可选取的随机数值
+     */
     private static final List<Float> X_MAX_CHOSE_RANDOMS = Arrays.asList(
-            0.01f,0.05f,0.1f,0.6f,0.11f, 0.16f, 0.21f, 0.26f, 0.31f, 0.7f, 0.75f, 0.8f, 0.85f, 0.87f);
-    /**y最多可选取的随机数值*/
+            0.01f, 0.05f, 0.1f, 0.6f, 0.11f, 0.16f, 0.21f, 0.26f, 0.31f, 0.7f, 0.75f, 0.8f, 0.85f, 0.87f);
+    /**
+     * y最多可选取的随机数值
+     */
     private static final List<Float> Y_MAX_CHOSE_RANDOMS = Arrays.asList(
-            0.01f,0.06f,0.11f, 0.17f, 0.23f, 0.29f, 0.35f, 0.41f, 0.47f, 0.53f, 0.59f, 0.65f, 0.71f, 0.77f, 0.83f);
-    /**x坐标当前可选的随机数组*/
+            0.01f, 0.06f, 0.11f, 0.17f, 0.23f, 0.29f, 0.35f, 0.41f, 0.47f, 0.53f, 0.59f, 0.65f, 0.71f, 0.77f, 0.83f);
+    /**
+     * x坐标当前可选的随机数组
+     */
     private List<Float> mXCurrentCanShoseRandoms = new ArrayList<>();
-    /**y坐标当前可选的随机数组*/
+    /**
+     * y坐标当前可选的随机数组
+     */
     private List<Float> mYCurrentCanShoseRandoms = new ArrayList<>();
 
-    /**已经选取x的随机数值*/
+    /**
+     * 已经选取x的随机数值
+     */
     private List<Float> mXRandoms = new ArrayList<>();
-    /**已经选取y的随机数值*/
+    /**
+     * 已经选取y的随机数值
+     */
     private List<Float> mYRandoms = new ArrayList<>();
 
 
@@ -71,7 +102,7 @@ public class WaterView extends FrameLayout {
     private boolean isCancelAnimtion;//是否销毁动画
     private int maxX, maxY;//子view的x坐标和y坐标的最大取值
     private float mMaxSpace;//父控件对角线的距离
-    private Point mDestroyPoint ;//view销毁时的点
+    private Point mDestroyPoint;//view销毁时的点
 
 
     public WaterView(@NonNull Context context) {
@@ -90,6 +121,7 @@ public class WaterView extends FrameLayout {
     @SuppressLint("HandlerLeak") private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
+            //根据isCancelAnimtion来标识是否退出，防止界面销毁时，再一次改变UI
             if (isCancelAnimtion) {
                 return;
             }
@@ -102,10 +134,14 @@ public class WaterView extends FrameLayout {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mMaxSpace = (float) Math.sqrt(w * w + h * h);
-        mDestroyPoint=new Point((int) getX(), h);
+        mDestroyPoint = new Point((int) getX(), h);
         maxX = w;
         maxY = h;
     }
+
+    /**
+     * 界面销毁时回调
+     */
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
@@ -137,6 +173,7 @@ public class WaterView extends FrameLayout {
         if (waters == null || waters.isEmpty()) {
             return;
         }
+        //确保初始化完成
         post(new Runnable() {
             @Override
             public void run() {
@@ -147,6 +184,7 @@ public class WaterView extends FrameLayout {
 
     /**
      * 设置数据
+     *
      * @param waters
      */
     private void setDatas(List<Water> waters) {
@@ -189,6 +227,7 @@ public class WaterView extends FrameLayout {
 
     /**
      * 添加显示动画
+     *
      * @param view
      */
     private void addShowViewAnimation(View view) {
@@ -224,8 +263,8 @@ public class WaterView extends FrameLayout {
      * @param view
      */
     private void setChildViewLocation(View view) {
-        view.setX((float) (maxX * getX_YRandom(mXCurrentCanShoseRandoms,mXRandoms)));
-        view.setY((float) (maxY * getX_YRandom(mYCurrentCanShoseRandoms,mYRandoms)));
+        view.setX((float) (maxX * getX_YRandom(mXCurrentCanShoseRandoms, mXRandoms)));
+        view.setY((float) (maxY * getX_YRandom(mYCurrentCanShoseRandoms, mYRandoms)));
         view.setTag(R.string.original_y, view.getY());
     }
 
@@ -234,13 +273,13 @@ public class WaterView extends FrameLayout {
      *
      * @return
      */
-    private double getX_YRandom(List<Float> choseRandoms,List<Float> saveRandoms) {
+    private double getX_YRandom(List<Float> choseRandoms, List<Float> saveRandoms) {
 
         if (choseRandoms.size() <= 0) {
             //防止水滴别可选项的个数还要多，这里就重新对可选项赋值
             setCurrentCanChoseRandoms();
         }
-        //取用一个随机数，就移除一个随机数
+        //取用一个随机数，就移除一个随机数，达到不用循环遍历来确保获取不一样的值
         float random = choseRandoms.get(mRandom.nextInt(choseRandoms.size()));
         choseRandoms.remove(random);
         saveRandoms.add(random);
@@ -273,28 +312,33 @@ public class WaterView extends FrameLayout {
     private void setOffSet() {
         for (int i = 0; i < mViews.size(); i++) {
             View view = mViews.get(i);
+            //拿到上次view保存的速度
             float spd = (float) view.getTag(R.string.spd);
+            //水滴初始的位置
             float original = (float) view.getTag(R.string.original_y);
             float step = spd;
             boolean isUp = (boolean) view.getTag(R.string.isUp);
             float translationY;
+            //根据水滴tag中的上下移动标识移动view
             if (isUp) {
                 translationY = view.getY() - step;
             } else {
                 translationY = view.getY() + step;
             }
-
+            //对水滴位移范围的控制
             if (translationY - original > CHANGE_RANGE) {
                 translationY = original + CHANGE_RANGE;
                 view.setTag(R.string.isUp, true);
             } else if (translationY - original < -CHANGE_RANGE) {
                 translationY = original - CHANGE_RANGE;
+                // FIXME:每次当水滴回到初始点时再一次设置水滴的速度，从而达到时而快时而慢
                 setSpd(view);
                 view.setTag(R.string.isUp, false);
             }
             view.setY(translationY);
         }
     }
+
 
     /**
      * 获取两个点之间的距离
@@ -316,9 +360,11 @@ public class WaterView extends FrameLayout {
     private void animRemoveView(final View view) {
         final float x = view.getX();
         final float y = view.getY();
+        //计算直线距离
         float space = getDistance(new Point((int) x, (int) y), mDestroyPoint);
 
         ValueAnimator animator = ValueAnimator.ofFloat(x, 0);
+        //根据距离计算动画执行时间
         animator.setDuration((long) (REMOVE_DELAY_MILLIS / mMaxSpace * space));
         animator.setInterpolator(new LinearInterpolator());
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -336,6 +382,7 @@ public class WaterView extends FrameLayout {
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
+                //结束时从容器移除水滴
                 removeView(view);
             }
         });
@@ -349,7 +396,7 @@ public class WaterView extends FrameLayout {
      * @param translationY
      * @param translationX
      */
-    private void setViewProperty( View view, float alpha,float translationY,float translationX) {
+    private void setViewProperty(View view, float alpha, float translationY, float translationX) {
         view.setTranslationY(translationY);
         view.setTranslationX(translationX);
         view.setAlpha(alpha);
